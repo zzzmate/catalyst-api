311ffe88abbee007c2f32e5cb5a48e23df692eaf
mate <104621347+zzzmate@users.noreply.github.com>
2026-07-31 23:33:18 +0200

    Add trust proxy setting and new mangadotnet route

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 5177;
 
+app.set("trust proxy", true);
+
 const mangafireRoutes = require("./routes/mangafire");
 const mangapillRoutes = require("./routes/mangapill");
+const mangadotnetRoutes = require("./routes/mangadotnet");
 
 app.get("/", (req, res) => {
   res.send("catalyst api started");
@@ -11,6 +14,7 @@ app.get("/", (req, res) => {
 
 app.use("/mangafire", mangafireRoutes);
 app.use("/mangapill", mangapillRoutes);
+app.use("/mangadotnet", mangadotnetRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
