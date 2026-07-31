1558acba5d7dc79a5b6bb5a873a0e164f75de5e7
mate <104621347+zzzmate@users.noreply.github.com>
2026-07-31 22:13:30 +0200

    Add mangapill routes to server
    Added mangapillRoutes to handle requests for the /mangapill endpoint.

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = 5177;
 
 const mangafireRoutes = require("./routes/mangafire");
+const mangapillRoutes = require("./routes/mangapill");
 
 app.get("/", (req, res) => {
   res.send("catalyst api started");
 });
 
 app.use("/mangafire", mangafireRoutes);
+app.use("/mangapill", mangapillRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
